9a4756a3375b6472658430227e3d40da98e6b115
dhruvy27 <98608767+dhruvy27@users.noreply.github.com>
2022-02-08 22:31:38 +0530

    Update ColorGame.java

--- ColorGamePracto/ColorGame.java
+++ ColorGamePracto/ColorGame.java
@@ -1,55 +1,55 @@
 package ColorGamePracto;
 import java.util.*;
 class ColorGame{
-    static thing b,i,s,f,sk,a,bl;
+    static properties b,i,s,f,sk,a,bl;
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
-        String st="";
-        Set<thing> Lists = new HashSet<thing>();
+        String item="";
+        Set<properties> Lists = new HashSet<properties>();
         Iterator value = Lists.iterator();
         
         while(true){
-            st=sc.nextLine();
-            if(st.charAt(0)=='+'){
+            item=sc.nextLine();
+            if(item.charAt(0)=='+'){
                 System.out.println("subscribed");
-                st=st.substring(1,st.length());
+                item=item.substring(1,item.length());
                 
-                switch(st){
-                case "banana":thing banana = new banana(); 
+                switch(item){
+                case "banana":properties banana = new banana(); 
                               Lists.add(banana);
                               b=banana;
                               break;
-                case "ink" :thing ink = new ink();
+                case "ink" :properties ink = new ink();
                             Lists.add(ink);
                             i=ink;
                             break;
-                case "salt":thing salt = new salt();
+                case "salt":properties salt = new salt();
                             Lists.add(salt);
                             s=salt;
                             break;
-                case "frog":thing frog = new frog();
+                case "frog":properties frog = new frog();
                             Lists.add(frog);
                             f=frog;
                             break;
-                case "blood":thing blood = new blood();
+                case "blood":properties blood = new blood();
                             Lists.add(blood);
                             bl=blood;
                             break;
-                case "sky": thing sky=new sky();
+                case "sky": properties sky=new sky();
                             Lists.add(sky);
                             sk=sky;
                             break;
-                case "apple":thing apple = new apple();
+                case "apple":properties apple = new apple();
                             Lists.add(apple);
                             a=apple;
                             break;
                 }
             }
-            if(st.charAt(0)=='-'){
+            if(item.charAt(0)=='-'){
                 System.out.println("unsubscribed");
-                st=st.substring(1,st.length());
+                item=item.substring(1,item.length());
                 
-                switch(st){
+                switch(item){
                 case "banana":Lists.remove(b);break;
                 case "ink" :Lists.remove(i);break;
                 case "salt":Lists.remove(s);break;
@@ -59,47 +59,47 @@ class ColorGame{
                 case "apple":Lists.remove(a);break;
                 }
             }
-            if(st.equals("list")){ 
+            if(item.equals("list")){ 
                    System.out.print("[");
-                for(thing list:Lists){
+                for(properties list:Lists){
                     System.out.print(list.getClass().getSimpleName()+" ");
                 };
                   System.out.println("]");
 
             }
-            if(st.equals("exit"))break;
-            switch(st){
-                case "green" :for(thing list:Lists){
-                    if(st.equals(list.getcolor1())||st.equals(list.getcolor2())){
+            if(item.equals("exit"))break;
+            switch(item){
+                case "green" :for(properties list:Lists){
+                    if(item.equals(list.getcolor1())||item.equals(list.getcolor2())){
                         list.sometime("green");
                     }
 
                 }
                 break;
-                case "yellow" :for(thing list:Lists){
-                    if(st.equals(list.getcolor1())||st.equals(list.getcolor2())){
+                case "yellow" :for(properties list:Lists){
+                    if(item.equals(list.getcolor1())||item.equals(list.getcolor2())){
                         if(list.equals(f))list.today("yellow");
                         else
                         list.sometime("yellow");
                     }
                 };break;
-                case "red":for(thing list:Lists){
-                    if(st.equals(list.getcolor1())||st.equals(list.getcolor2())){
+                case "red":for(properties list:Lists){
+                    if(item.equals(list.getcolor1())||item.equals(list.getcolor2())){
                         list.sometime("red");
                     }
                 }break;
-                case "black":for(thing list:Lists){
-                    if(st.equals(list.getcolor1())||st.equals(list.getcolor2())){
+                case "black":for(properties list:Lists){
+                    if(item.equals(list.getcolor1())||item.equals(list.getcolor2())){
                         list.sometime("black");
                     }
                 }break;
-                case "white":for(thing list:Lists){
-                    if(st.equals(list.getcolor1())||st.equals(list.getcolor2())){
+                case "white":for(properties list:Lists){
+                    if(item.equals(list.getcolor1())||item.equals(list.getcolor2())){
                         list.sometime("white");
                     }
                 }break;
-                case "blue":for(thing list:Lists){
-                    if(st.equals(list.getcolor1())||st.equals(list.getcolor2())){
+                case "blue":for(properties list:Lists){
+                    if(item.equals(list.getcolor1())||item.equals(list.getcolor2())){
                         if(list.equals(f))list.today("blue");
                         else
                         list.sometime("blue");
@@ -109,17 +109,17 @@ class ColorGame{
             }
         }
     }
-    public interface thing{
+    public interface properties{
         public String getcolor1();
         public String getcolor2();
         public default void sometime(String color){
-            System.out.println(String.format("I'm %s! I'm sometimes %s!",getClass().getSimpleName(),color));
+            System.out.println(String.format(" Hi I'm %s! I'm sometimes %s!",getClass().getSimpleName(),color));
         }
         public default void today(String color){
-            System.out.println(String.format("I'm %s! I'm %s today!",getClass().getSimpleName(),color));
+            System.out.println(String.format("Heloo I'm %s! I'm %s today!",getClass().getSimpleName(),color));
         }
     }
-    public static class banana implements thing{
+    public static class banana implements properties{
         public String getcolor1(){
             return "green";
         }
@@ -127,7 +127,7 @@ class ColorGame{
             return "yellow";
         }
     }
-    public static class ink implements thing{
+    public static class ink implements properties{
         
         public String getcolor1(){
             return "red";
@@ -136,7 +136,7 @@ class ColorGame{
             return "black";
         }
     }
-    public static class salt implements thing{
+    public static class salt implements properties{
         public String getcolor1(){
             return "white";
         }
@@ -144,7 +144,7 @@ class ColorGame{
             return "white";
         }
     }
-    public static class frog implements thing{
+    public static class frog implements properties{
         public String getcolor1(){
             return "blue";
         }
@@ -152,7 +152,7 @@ class ColorGame{
             return "yellow";
         }
     }
-    public static class blood implements thing{
+    public static class blood implements properties{
         public String getcolor1(){
             return "red";
         }
@@ -161,7 +161,7 @@ class ColorGame{
         }
         
     }
-    public static class sky implements thing{
+    public static class sky implements properties{
         public String getcolor1(){
             return "blue";
         }
@@ -169,7 +169,7 @@ class ColorGame{
             return "black";
         }
     }
-    public static class apple implements thing{
+    public static class apple implements properties{
         public String getcolor1(){
             return "green";
         }
